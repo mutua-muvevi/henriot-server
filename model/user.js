59e06e27a5b91529390db7568e5e242c4fb071f7
SamsAgency <samstore637@gmail.com>
2022-07-14 15:35:26 +0300

    user schema reusable methods

--- model/user.js
+++ model/user.js
@@ -42,5 +42,45 @@ const UserSchema = new mongoose.Schema({
 	expireAfterSeconds: 86400
 });
 
-const User = mongoose.model("User", UserSchema);
+
+// hashing the passowrds
+UserSchema.pre("save", async function(next){
+	if(!this.isModified("password")){
+		next()
+	}
+
+	const salt = await bcrypt.genSalt(10)
+	this.password = await bcrypt.hash(this.password, salt)
+	next()
+})
+
+// comparing passwords
+UserSchema.methods.comparePasswords = async function(password){
+	return await bcrypt.compare(password, this.password)
+}
+
+// generating json tokens
+UserSchema.methods.genToken = function(){
+	return jwt.sign(
+		{id: this._id, authorization: this.authorization},
+		process.env.JWT_SECRET,
+		{ expiresIn: process.env.JWT_EXPIRY }
+	)
+}
+
+// generating reset token
+UserSchema.methods.genResetToken = function(){
+	const resetToken = crypto.randomBytes(10).toString("hex")
+
+	this.resetPasswordToken = crypto
+		.createHash("sha256")
+		.update(resetToken)
+		.digest("hex")
+	
+	this.resetPasswordExpiry = Date.now() + 240 * (60 * 1000)
+	return resetToken
+}
+
+// the model
+const User = mongoose.model("User", UserSchema)
 module.exports = User
