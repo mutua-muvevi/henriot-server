f6a9934b4e7e7b01cf4396e35a809c31f9058a55
samdev <samstore637@gmail.com>
2023-07-27 14:38:17 +0300

    added sparkpost

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,35 +1,18 @@
-const nodemailer = require("nodemailer");
-const logger = require("./logger");
+const SparkPost = require("sparkpost");
+const client = new SparkPost(process.env.SENDMAILAPIKEY, {origin: 'https://api.sparkpost.com'});
 
-const sendEmail = (options) => {
-	return new Promise((resolve, reject) => {
-		// the transporter that will send the mail
-		const transporter = nodemailer.createTransport({
-			service: process.env.EMAIL_SERVICE,
-			auth: {
-				user: process.env.EMAIL_USERNAME,
-				pass: process.env.EMAIL_PASSWORD,
-			},
-		});
-
-		// mail options
-		const mailOptions = {
-			from: process.env.EMAIL_FROM,
-			to: options.to,
-			subject: options.subject,
-			html: options.html,
-		};
-
-		transporter.sendMail(mailOptions, (error, info) => {
-			if (error) {
-				logger.error(`Send Mail Error : ${JSON.stringify(error)}`);
-				reject(error);
-			} else {
-				logger.info(`Email sent successfully`);
-				resolve(info);
-			}
-		});
+const SendEmail = async ({ to, from, subject, html }) => {
+	const response = await client.transmissions.send({
+		content: {
+			from,
+			subject,
+			html,
+		},
+		recipients: [{ address: to }],
 	});
+
+	return response;
 };
 
-module.exports = sendEmail;
+module.exports = SendEmail ;
+
