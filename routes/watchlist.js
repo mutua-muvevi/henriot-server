52e836f95e9555a1a12cab10b1a93e6c5b235f83
mutua-muvevi <samstore637@gmail.com>
2023-09-29 02:37:57 +0300

    added watchlist ite,

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { authMiddleware } = require("../middleware/auth");
 const { newWatchlistItem } = require("../controller/watchlist/new");
+const { deleteWatchList, deleteSymbolFromWatchList } = require("../controller/watchlist/delete");
 // const { editOrder } = require("../controller/watchlist/edit");
 // const { authMiddleware } = require("../middleware/auth");
 // const { deleteAllOrders, deleteSingleOrder } = require("../controller/watchlist/delete");
@@ -10,6 +11,7 @@ const { newWatchlistItem } = require("../controller/watchlist/new");
 router.route("/new").post(authMiddleware, newWatchlistItem);
 // router.route("/edit/:orderID").post(authMiddleware, editOrder);
 // router.route("/delete/all").post(authMiddleware, deleteAllOrders);
-// router.route("/delete/single/:orderID").post(authMiddleware, deleteSingleOrder);
+router.route("/delete/single/:accountID/:userID/:watchlistID").post(authMiddleware, deleteWatchList);
+router.route("/delete/single/:accountID/:userID/:watchlistID/:symbol").post(authMiddleware, deleteSymbolFromWatchList);
 
 module.exports = router
