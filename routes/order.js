7ac00dc37f3a817fb43e9537501133448bdc92a8
mutua-muvevi <samstore637@gmail.com>
2023-09-29 02:57:38 +0300

    fetch watchlist

--- routes/order.js
+++ routes/order.js
@@ -5,10 +5,15 @@ const { newOrder } = require("../controller/order/new");
 const { editOrder } = require("../controller/order/edit");
 const { authMiddleware } = require("../middleware/auth");
 const { deleteAllOrders, deleteSingleOrder } = require("../controller/order/delete");
+const { fetchAllOrders, fetchAllIndividualOrders } = require("../controller/order/fetch");
 
 router.route("/new").post(authMiddleware, newOrder);
-router.route("/edit/:orderID").post(authMiddleware, editOrder);
-router.route("/delete/all").post(authMiddleware, deleteAllOrders);
-router.route("/delete/single/:orderID").post(authMiddleware, deleteSingleOrder);
+router.route("/edit/:orderID").put(authMiddleware, editOrder);
+
+router.route("/fetch/all/:userID").get(authMiddleware, fetchAllOrders);
+router.route("/fetch/all/:userID/:accountID").get(authMiddleware, fetchAllIndividualOrders);
+
+router.route("/delete/all").delete(authMiddleware, deleteAllOrders);
+router.route("/delete/single/:orderID").delete(authMiddleware, deleteSingleOrder);
 
 module.exports = router
